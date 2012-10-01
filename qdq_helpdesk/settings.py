f0d521a21f108423962a62c77265900bc744475c
Arturo Fernandez <arturo@bsnux.com>
2012-10-01 15:45:35 +0200

    Adding TEMPLATE_CONTEXT_PROCESSORS

--- qdq_helpdesk/settings.py
+++ qdq_helpdesk/settings.py
@@ -62,6 +62,17 @@ QDQAUTHMIDDLEWARE = {
 }
 
 
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.core.context_processors.request',
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.static',
+    'django.core.context_processors.tz',
+    'django.contrib.messages.context_processors.messages',
+)
+
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
